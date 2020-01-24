aef44b496a4358a7074592678789f896907d0b59
Darren <darren@darrenmothersele.com>
2020-01-24 08:33:24 +0000

    add index-dev.html and fix margin on pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ import { range } from 'lodash';
       </div>
       <div class="max-w-sm mx-auto mt-8 flex justify-between items-center">
           <button (click)="prevPage()">
-              <svg class="mt-2 fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <svg class="fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                   <path d="M3.828 9l6.071-6.071-1.414-1.414L0 10l.707.707 7.778 7.778 1.414-1.414L3.828 11H20V9H3.828z"/>
               </svg>
           </button>
@@ -45,7 +45,7 @@ import { range } from 'lodash';
               </ul>
           </div>
           <button (click)="nextPage()">
-              <svg class="mt-2 fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <svg class="fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                   <path d="M16.172 9l-6.071-6.071 1.414-1.414L20 10l-.707.707-7.778 7.778-1.414-1.414L16.172 11H0V9z"/>
               </svg>
           </button>
